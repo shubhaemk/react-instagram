6a263669745ab343aea9339c466f56f786cea07f
Shubham Kamath <dev.shubhaem@gmail.com>
2018-12-22 01:14:13 +0530

    more minor changes

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,8 +4,18 @@ import { Button, Form, FormGroup, Input, Container, Col ,Label, Jumbotron  } fro
 const Home = (props) => {
   return (
     <Jumbotron>
-      <Form>
-        <Input></Input>
+      <Form inline>
+        <FormGroup>
+          <Label for="exampleEmail" hidden>Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+        </FormGroup>
+        {' '}
+        <FormGroup>
+          <Label for="examplePassword" hidden>Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+        </FormGroup>
+        {' '}
+        <Button>Submit</Button>
       </Form>
     </Jumbotron>
   );
